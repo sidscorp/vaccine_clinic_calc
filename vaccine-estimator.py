36b49b2b9ea50c92a36ca16a1e7fe39c9b31ef05
Sidd Nambiar <siddharthanambiar@gmail.com>
2021-02-06 14:38:24 -0500

    Update Simulation

--- vaccine-estimator.py
+++ vaccine-estimator.py
@@ -1,156 +1,142 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  5 18:06:54 2021
-
-@author: siddh
-"""
-
+import random
+import simpy
+import numpy as np
 import math
 import streamlit as st
 
-class MMcQueue(object):
-    def __init__(self, arrival, departure, capacity):
-        """
-        Given the parameter of one M/M/c/c Queue, 
-        initialize the queue with these parameter and calculate some parameters.
-        `_rou`:     Server Utilization
-        `_p0`:      Probability of that there is no packets in the queue
-        `_pc`:      Probability of that there is exactly `capacity` packets in the queue,
-                    that is, all the server is busy.
-        `_probSum`:  p0 + p1 + p2 + ... pc - pc
-        `_finalTerm`: 1/(c!) * (arrival / departure)^c
-        """
-        if capacity * departure <= arrival:
-            raise ValueError("This Queue is unstable with the Input Parameters!!!")
-        self._arrival = float(arrival)
-        self._departure = float(departure)
-        self._capacity = capacity
-        self._rou = self._arrival / self._departure / self._capacity
+def run_vaccination_simulation(RANDOM_SEED, NUM_CHECKIN, CHECKIN_TIME, PATIENT_INTER, SIM_TIME, NUM_VACCINATORS, VACCINATION_TIME, NUM_ADVERSEWAIT, ADVERSEWAIT_TIME):
+    output_checkin_waittime = []
+    output_checkin_waitnum = []
+    output_vaccination_waittime = []
+    output_vaccination_waitnum = []
+    output_adverse_waittime = []
+    output_adverse_waitnum = []
+    output_total_facility_time = []
+    my_bar = st.progress(0)
+    
+    for replication in range(0,30):
+        percent_complete = math.ceil(100*(replication/30))
+        facility_arrival_times = []
+        checkin_begin_times = []
+        checkin_end_times = []
+        vaccination_begin_times = []
+        vaccination_end_times = []
+        adverse_begin_times = []
+        adverse_end_times = []
+        facility_departure_times = []
 
-        # init the parameter as if the capacity == 0
-        powerTerm = 1.0
-        factorTerm = 1.0
-        preSum = 1.0
-        # Loop through `1` to `self._capacity` to get each term and preSum
-        for i in range(1, self._capacity + 1):
-            powerTerm *= self._arrival / self._departure
-            factorTerm /= i
-            preSum += powerTerm * factorTerm
-        self._finalTerm = powerTerm * factorTerm
-        preSum -= self._finalTerm
-        self._p0 = 1.0 / (preSum + self._finalTerm / (1 - self._rou))
-        self._pc = self._finalTerm * self._p0
-        self._probSum = preSum * self._p0
+        class Vaccination_Clinic(object):
+            def __init__(self, env, num_checkin, checkin_time, num_vaccinators, vaccination_time, num_adversewait, adversewait_time):
+                self.env = env
+                self.checkin_personnel = simpy.Resource(env, num_checkin)
+                self.checkintime = checkin_time
+                self.vaccination_booth = simpy.Resource(env, num_vaccinators)
+                self.vaccinationtime = vaccination_time
+                self.adverse_event_spot = simpy.Resource(env, num_adversewait)
+                self.adversewaittime = adversewait_time
 
-
-    @property
-    def arrival(self):
-        return self._arrival
-
-    @property
-    def departure(self):
-        return self._departure
-
-    @property
-    def capacity(self):
-        return self._capacity
-
-    def getPk(self, k):
-        """
-        Return the probability when there are `k` packets in the system
-        """
-        if k == 0:
-            return self._p0
-        elif k == self._capacity:
-            return self._pc
-        elif k < self._capacity:
-            factorTerm = 1.0 / math.factorial(k)
-            powerTerm = math.pow(self._arrival / self._departure, k)
-            return self._p0 * factorTerm * powerTerm
-        else:
-            return self._finalTerm * math.pow(self._rou, k - self._capacity) * self._p0
-
-    def getQueueProb(self):
-        """
-        Return the probability when a packet comes, it needs to queue in the buffer.
-        That is, P(W>0) = 1 - P(N < c)
-        Also known as Erlang-C function
-        """
-        return 1.0 - self._probSum
-
-    def getIdleProb(self):
-        """
-        Return the probability when the sever is idle.
-        That is , P(N=0)
-        """
-        return self._p0
-
-    def getAvgPackets(self):
-        """
-        Return the average number of packets in the system (in service and in the queue)
-        """
-        return self._rou / (1 - self._rou) * self.getQueueProb() + self._capacity * self._rou
-
-    def getAvgQueueTime(self):
-        """
-        Return the average time of packets spending in the queue
-        """
-        return self.getQueueProb() / (self._capacity * self._departure - self._arrival)
-
-    def getAvgQueuePacket_Given(self):
-        """
-        Given there is packet in the queue,
-        return the average number of packets in the queue
-        """
-        return self._finalTerm * self._p0 / (1.0 - self._rou) / (1.0 - self._rou)
-
-    def getAvgQueueTime_Given(self):
-        """
-        Given a packet must wait, 
-        return the average time of this packet spending in the queue
-        """
-        if self.getQueueProb() == 0:
-            return 0
-        return self.getAvgQueuePacket_Given() / (self.getQueueProb() * self._arrival)
-
-    def getAvgResponseTime(self):
-        """
-        Return the average time of packets spending in the system (in service and in the queue)
-        """
-        return self.getAvgQueueTime() + 1.0 / self._departure
-
-    def getAvgPacketInSystem(self):
-        """
-        Return the average number of packets in the system.
-        """
-        return self.getAvgResponseTime() * self._arrival
-
-    def getAvgBusyServer(self):
-        """
-        Return the average number of busy Server.
-        """
-        return self.arrival / self.departure
+            def checkin(self, patient):
+                yield self.env.timeout(np.random.triangular(CHECKIN_TIME - 1, CHECKIN_TIME, CHECKIN_TIME + 1))
 
 
-    def getPorbWhenQueueTimeLargerThan(self, queueTime):
-        """
-        Return the probability when the queuing time of the packet is larger than `queueTime`
-        That is P(W > queueTime) = 1 - P(W <= queueTime)
-        """
-        firstTerm = self._pc / (1.0 - self._rou)
-        expTerm = - self._capacity * self._departure * (1.0 - self._rou) * queueTime
-        secondTerm = math.exp(expTerm)
-        return firstTerm * secondTerm
+            def vaccinate(self, patient):
+                yield self.env.timeout(np.random.triangular(VACCINATION_TIME - 1, VACCINATION_TIME, VACCINATION_TIME + 1))
+
+            def monitor_adverse(self, patient):
+                yield self.env.timeout(ADVERSEWAIT_TIME)
+
+        def patient(env, name, vac):
+            #print('%s arrives at the vaccination clinic at %.2f.' % (name, env.now))
+            facility_arrival_times.append(env.now)
+            with vac.checkin_personnel.request() as request:
+                yield request
+
+                #print("%s arrives at checkin counter" % name)
+                checkin_begin_times.append(env.now)
+                yield env.process(vac.checkin(name))
+                checkin_end_times.append(env.now)
+                #print("%s completes check-in at %.2f." % (name, env.now))
+
+            with vac.vaccination_booth.request() as request:
+                yield request
+                vaccination_begin_times.append(env.now)
+                #print("%s arrives at vaccination booth" % name)
+                yield env.process(vac.vaccinate(name))
+                vaccination_end_times.append(env.now)
+
+                #print("%s gets shot in the arm at %.2f." % (name, env.now))
+
+            with vac.adverse_event_spot.request() as request:
+                yield request
+                adverse_begin_times.append(env.now)
+                #print("%s proceeds to wait to monitor for adverse events" % name)
+                yield env.process(vac.monitor_adverse(name))
+                adverse_end_times.append(env.now)
+                facility_departure_times.append(env.now)
+                #print("%s leaves facility safely at %.2f." % (name, env.now))
+
+        def setup(env, num_checkin, checkin_time, num_vaccinators, vaccination_time, num_adversewait, adversewait_time, patient_inter):
+            vaccinationclinic = Vaccination_Clinic(env, num_checkin, checkin_time, num_vaccinators, vaccination_time, num_adversewait, adversewait_time)
+            i = 0
+            while True:
+                yield env.timeout(np.random.exponential(scale=patient_inter))
+                i += 1
+                env.process(patient(env, 'Patient %d' % i, vaccinationclinic))   
 
 
-def calc_metrics(lambd, mu, c):
-    thisQueue = MMcQueue(lambd,mu,c)
-    Wq = thisQueue.getAvgQueueTime()
-    Lq = lambd*Wq
-    server_busy = thisQueue.getAvgBusyServer()/c
-    L = thisQueue.getAvgPacketInSystem()
-    W = L/lambd
-    return [L, W, Lq, Wq, server_busy]
+        random.seed(RANDOM_SEED)
+
+        # Create an environment and start the setup process
+        env = simpy.Environment()
+        env.process(setup(env, NUM_CHECKIN, CHECKIN_TIME, NUM_VACCINATORS, VACCINATION_TIME, NUM_ADVERSEWAIT, ADVERSEWAIT_TIME, PATIENT_INTER))
+
+        # Execute!
+        env.run(until=SIM_TIME)
+        average_facility_total_time = np.mean([facility_departure_times[i] - facility_arrival_times[i] for i in range(len(facility_departure_times))])
+        #print("Approximate total time at facility is %.1f mins." % average_facility_total_time)
+        average_checkin_wait_time = np.mean([checkin_begin_times[i] - facility_arrival_times[i] for i in range(len(checkin_begin_times))])
+        #print("Approximate wait time between arrival and checkin is %.1f mins." % average_checkin_wait_time)
+        average_vaccination_wait_time = np.mean([vaccination_begin_times[i] - checkin_end_times[i] for i in range(len(vaccination_begin_times))])
+        #print("Approximate wait time between checkin and getting vaccinated is %.1f mins." % average_vaccination_wait_time)
+        average_adverse_wait_time = np.mean([adverse_begin_times[i] - vaccination_end_times[i] for i in range(len(adverse_begin_times))])
+        #print("Approximate wait time between getting vaccine and finding adverse monitoring wait spot is %.1f mins." % average_adverse_wait_time)
+
+        avg_waiting_checkin = []
+        for i in [x * 0.1 for x in range(0, SIM_TIME)]:
+            num_arrived_facility = sum(1 for j in facility_arrival_times if j <= i)
+            num_started_checin = sum(1 for j in checkin_begin_times if j <= i)
+            avg_waiting_checkin.append(num_arrived_facility - num_started_checin)
+        #print("Approximate # of patients waiting to checkin at any time is %.1f." % np.mean(avg_waiting_checkin))
+
+        avg_waiting_vaccine = []
+        for i in [x * 0.1 for x in range(0, SIM_TIME)]:
+            num_finished_checin = sum(1 for j in checkin_end_times if j <= i)
+            num_started_vaccine = sum(1 for j in vaccination_begin_times if j <= i)
+            avg_waiting_vaccine.append(num_finished_checin - num_started_vaccine)
+        #print("Approximate # of patients waiting between checkin and vaccine at any time is %.1f." % np.mean(avg_waiting_vaccine))
+
+        avg_waiting_adverse = []
+        for i in [x * 0.1 for x in range(0, SIM_TIME)]:
+            num_finished_vaccine = sum(1 for j in vaccination_end_times if j <= i)
+            num_started_adverse = sum(1 for j in adverse_begin_times if j <= i)
+            avg_waiting_adverse.append(num_finished_vaccine - num_started_adverse)
+        #print("Approximate # of patients waiting between checkin and vaccine at any time is %.1f." % np.mean(avg_waiting_vaccine))
+
+        output_checkin_waittime.append(average_checkin_wait_time)
+        output_checkin_waitnum.append(np.mean(avg_waiting_checkin))
+        output_vaccination_waittime.append(average_vaccination_wait_time)
+        output_vaccination_waitnum.append(np.mean(avg_waiting_vaccine))
+        output_adverse_waittime.append(average_adverse_wait_time)
+        output_adverse_waitnum.append(np.mean(avg_waiting_adverse))
+        output_total_facility_time.append(average_facility_total_time)
+    
+        my_bar.progress(percent_complete + 1)
+        
+    return [np.mean(output_checkin_waittime), np.mean(output_checkin_waitnum), np.mean(output_vaccination_waittime), 
+            np.mean(output_vaccination_waitnum), np.mean(output_adverse_waittime), np.mean(output_adverse_waitnum),
+            np.mean(output_total_facility_time)]
+
+
 
 st.title('Vaccine Clinic Scheduling & Staffing Calculator')
 
@@ -165,18 +151,17 @@ num_waiting_area_adverse = st.number_input("Input the number of waiting spots av
 
 
 if(st.button('Calculate Metrics')): 
-    lambd = num_arrive_hour/60
-    mu_checkin = 1
-    mu_vaccinate = 4
-    if(lambd/(mu_checkin*num_checkin) >= 1):
-        st.warning("Warning: Unstable queue. Increase number of check-in personnel.")
-    elif(lambd/(mu_vaccinate*num_vaccine_booths) >= 1):
-        st.warning("Warning: Unstable queue. Increase number of vaccination booths.")
-    else:
-        [L_checin, W_checkin, Lq_checkin, Wq_checkin, Busy_checkin] = calc_metrics(lambd, mu_checkin, num_checkin)
-        [L_vaccine, W_vaccine, Lq_vaccine, Wq_vaccine, Busy_vaccine] = calc_metrics(lambd, mu_vaccinate, num_vaccine_booths)
-        total_time = W_checkin + W_vaccine + 15
-        st.text("Patients can expect to be in the facility for {:0.1f} mins.".format(total_time)) 
-        st.text("Vaccinators are busy for approximately {:0.1f} % of time".format(Busy_vaccine*100)) 
-        st.text("Checkin personnel are busy for approximately {:0.1f} % of time".format(Busy_checkin*100)) 
-        st.text("Approximately {:0.1f} individuals can expect to be waiting in line at check-in".format(Lq_checkin)) 
+    RANDOM_SEED = 42
+    NUM_CHECKIN = 3
+    CHECKIN_TIME = 1
+    PATIENT_INTER = 60/num_arrive_hour
+    SIM_TIME = 60*8
+    NUM_VACCINATORS = 8
+    VACCINATION_TIME = 4
+    NUM_ADVERSEWAIT = 35
+    ADVERSEWAIT_TIME = 15
+    [avg_checkin_waitT, avg_checkin_waitN, avg_vaccine_waitT, avg_vaccine_waitN, avg_adverse_waitT, avg_adverse_waitN, avg_total_time] = run_vaccination_simulation(RANDOM_SEED, NUM_CHECKIN, CHECKIN_TIME, PATIENT_INTER, SIM_TIME, NUM_VACCINATORS, VACCINATION_TIME, NUM_ADVERSEWAIT, ADVERSEWAIT_TIME)
+    st.text("Patients can expect to be in the facility for {:0.1f} mins.".format(avg_total_time)) 
+    st.text("Approximately {:0.1f} patients must wait before check-in".format(avg_checkin_waitN)) 
+    st.text("Patients can expect to wait for approximately {:0.1f} mins to check-in".format(avg_checkin_waitT))
+    st.text("Approximately {:0.1f} patients must wait between check-in and getting vaccine".format(avg_vaccine_waitN)) 
